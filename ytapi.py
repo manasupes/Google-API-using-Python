2dd63479277980c679b0653a30867c60e2ca6450
manasupes <34783681+manasupes@users.noreply.github.com>
2020-05-30 16:01:35 +0530

    Update ytapi.py

--- ytapi.py
+++ ytapi.py
@@ -6,7 +6,7 @@ youtube = build('youtube', 'v3',developerKey=api_key)
 
 request = youtube.channels().list(
 		part='statistics',
-		forUsername='#your User Name'
+		forUsername='#your channel User Name'
 		)
 
 response = request.execute()
